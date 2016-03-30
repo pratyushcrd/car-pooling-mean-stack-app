4496058fa88c4190c54eeca78a2aba9d8b6f724e
pratyush <pratyush.creed@gmail.com>
2016-03-30 21:45:47 +0530

    Angular setup and home view

--- server/models/city.js
+++ server/models/city.js
@@ -10,7 +10,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CitySchema = new Schema({
-    city: {type: String, required: true, index: {unique: true}}
+    city: {type: String, required: true, index: {unique: true}},
+    created_at: {type: Date, default: Date.now()}
 });
 
 
