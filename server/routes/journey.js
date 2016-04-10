8230434800a17b580d14049e48b9f6826f911e38
isle-prashant <isle.prashant@hotmail.com>
2016-04-10 19:23:24 +0530

    add journey

--- server/routes/journey.js
+++ server/routes/journey.js
@@ -5,6 +5,6 @@ var express = require('express');
 var router = express.Router();
 var Car = require('../models/car');
 var Journey = require('../models/journey');
-/* GET users listing. */
+/* GET users listing . */
 
 module.exports = router;
