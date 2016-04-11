51483de9677d5908637627418d227b2efd769c0c
pratyush <pratyush.creed@gmail.com>
2016-04-11 18:06:18 +0530

    journey form

--- client/assets/js/angular/app.js
+++ client/assets/js/angular/app.js
@@ -3,7 +3,7 @@
  */
 var app = angular.module('cityCommute', ['ngResource', 'ngRoute']);
 app.factory('Journey', function($resource) {
-    return $resource('/api/journey/:id', null, {
+    return $resource('/api/journeys/:id', null, {
         'update': {
             method: 'PUT'
         }
@@ -12,14 +12,35 @@ app.factory('Journey', function($resource) {
 /* Controller for index page */
 app.controller('HomeController', function($scope, $http, $timeout) {});
 /* Controller for index page */
-app.controller('JourneyController', function($scope, $http, $timeout) {});
-    /* Configuing routes */
+app.controller('JourneyController', function($scope, $http, $timeout, Journey) {
+    // Object to store form data
+    $scope.journeyObject = {};
+    $scope.postJourney = function() {
+        var newJourney = new Journey();
+        newJourney.startStreet = $scope.journeyObject.startStreet;
+        newJourney.startArea = $scope.journeyObject.startArea;
+        newJourney.endStreet = $scope.journeyObject.endStreet;
+        newJourney.endArea = $scope.journeyObject.endArea;
+        newJourney.departure = $scope.journeyObject.departure;
+        newJourney.vehicle = $scope.journeyObject.vehicle;
+        newJourney.availableSeats = $scope.journeyObject.availableSeats;
+        newJourney.genderPreference = $scope.journeyObject.genderPreference;
+        newJourney.description = $scope.journeyObject.description;
+        newJourney.fare = $scope.journeyObject.fare;
+        newJourney.$save($scope.journeyObject, function(tweet) {
+            console.log(tweet);
+            
+        }, function(err) {
+            console.log(err);
+        })
+    }
+});
+/* Configuing routes */
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         templateUrl: '/home.html',
         controller: 'HomeController'
-    })
-    .when('/addjourney', {
+    }).when('/addjourney', {
         templateUrl: '/post_journey.html',
         controller: 'JourneyController'
     }).otherwise({
