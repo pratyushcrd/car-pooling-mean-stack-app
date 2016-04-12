7650a00aeb4c8f03dc2e29a81ee921419d797df7
pratyush <pratyush.creed@gmail.com>
2016-04-12 22:09:31 +0530

    minor design change homepage

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //mongoose
-mongoose.connect('mongodb://localhost/db23ksf');
+mongoose.connect('mongodb://localhost/db23ksfklsfn');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
