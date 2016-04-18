2a3286e6f955a1eb66ac41d657c3db1117bd7fda
isle-prashant <isle.prashant@hotmail.com>
2016-04-19 01:47:03 +0530

    Chat api implemented

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var Chat = require('../models/chat');
+var Journey = require('../models/journey');
 
 var ifLoggedIn = function (req, res, next) {
     if (req.user) {
@@ -12,33 +13,49 @@ var ifLoggedIn = function (req, res, next) {
     });
 };
 
-router.get('/', ifLoggedIn, function (req, res, next) {
-    Chat.find({senderId: req.user._id, receiverId: req.user._id}, {receiverId: req.user._id}, function (err, messages) {
+router.get('/:jid', ifLoggedIn, function (req, res, next) {
+    Chat.find({journeyId: req.params.jid}, function (err, messages) {
         if (err) {
             return res.send({error: err});
         }
         res.send(messages);
-    }).sort({_id: -1});
+    }).sort({created_at: -1});
 });
 
-router.post('/', ifLoggedIn, function (req, res, next) {
-    User.findOne({_id: req.body.receiverId}, function (err, user) {
-        if (err) {
-            return res.json({error: "User dose not exists"});
+router.post('/:jid', ifLoggedIn, function (req, res, next) {
+    Journey.findOne({_id: req.params.jid}, function (err, journey) {
+        if (err || !journey) {
+            return res.json({error: "Journey dose not exists"});
+        }
+        else {
+            var flag = 0;
+            for (index in req.user.journeys) {
+                var userJourney = req.user.journeys[index];
+                console.log(userJourney);
+                console.log(req.params.jid);
+                if (userJourney == req.params.jid) {
+                    flag = 1;
+                }
+            }
+            if (flag == 1) {
+                var newMessage = new Chat();
+                newMessage.message = req.body.message;
+                newMessage.userId = req.user._id;
+                newMessage.journeyId = req.params.jid;
+                newMessage.save(function (err, Message) {
+                    if (err) {
+                        return res.send({error: err});
+
+                    }
+                    return res.send(Message);
+                });
+            }
+            else {
+                return res.send({error: "You are not authorized"});
+            }
+
         }
 
-        var newMessage = Chat();
-        newMessage.message = req.body.message;
-        newMessage.senderId = req.user._id;
-        newMessage.receiverId = req.body.receiverId;
-        newMessage.save(function (err, MessageDetail) {
-            if (err) {
-                res.send(err);
-                return next();
-
-            }
-            res.send(MessageDetail);
-        });
 
     });
 
